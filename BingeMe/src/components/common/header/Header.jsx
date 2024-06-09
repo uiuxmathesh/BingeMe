6de026975d4fe86a0a9ddf1f26b44a6809ed8390
uiuxmathesh <matheshpremkumar@gmail.com>
2024-06-09 08:28:33 +0530

    Navbar enhancememmt

--- BingeMe/src/components/common/header/Header.jsx
+++ BingeMe/src/components/common/header/Header.jsx
@@ -1,12 +1,31 @@
 import "./Header.css"
 import logo from "../../../assets/logo.png"
+import Buttons from "../buttons/Buttons"
 const Header = () => {
+  const PageNames = ["Home", "About us", "Contact"]
   return (
     <header>
         <div className="branding">
             <img className="brand-logo" src={logo}  alt="BingeMe logo" />
             <h1 className="brand-name">Binge Me!</h1>
         </div>
+        <nav>
+          <ul className="nav-links">
+            {
+              PageNames.map((page)=><li className="nav-item" key={page}>{page}</li>)
+            }
+          </ul>
+
+          <div className="action-buttons">
+            <div id="nav-search">
+             <input type="text" placeholder="Search" />
+             <span className="material-symbols-outlined">search</span>
+            </div>
+            <div className="button-div">
+              <Buttons name="Sign in"/>
+            </div>
+          </div>
+        </nav>
     </header>
   )
 }
